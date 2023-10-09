b66b5d74d7e3616ea7e9d0782b85f1788b9f3259
Alain Serrano <alains1984@gmail.com>
2023-10-09 23:35:29 +0200

    Adds album, start_time, end_time fields to Solo

--- solos/tests/test_models.py
+++ solos/tests/test_models.py
@@ -8,11 +8,15 @@ class SoloModelTestCase(TestCase):
         self.solo  = Solo.objects.create(
             track='Jerusalem',
             artist='Bob Marley',
-            instrument='Guitar'
+            instrument='Guitar',
+            album='Festival Pop',
+            start_time='1:24',
+            end_time='4:06'
         )
     
     def test_solo_basic(self):
         """Test the basic functionality of Solo
         """
         self.assertEqual(self.solo.artist, 'Bob Marley')
+        self.assertEqual(self.solo.end_time, '4:06')
         
